a07d9624c59db3ffab3743e909467a343ab829f0
Johnny Hsu <a209328641@gmail.com>
2022-11-06 18:45:17 +0800

    rewrite cart-slice, to store only the amount of cart products for redux state

--- src/components/header/NavBar.tsx
+++ src/components/header/NavBar.tsx
@@ -5,10 +5,7 @@ import Button from "../ui/Button";
 
 const NavBar: React.FC = () => {
     const cart = useSelector((state: RootState) => state.cart);
-    const productAmountInCart = cart.products.reduce(
-        (accu, curr) => accu + curr.amount,
-        0
-    );
+    const productAmountInCart = cart.amountOfCartProducts;
 
     return (
         <div className={s["nav-bar"]}>
@@ -51,7 +48,7 @@ const NavBar: React.FC = () => {
                                 d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
                             />
                         </svg>
-                        {cart.totalAmount > 0 && (
+                        {cart.amountOfCartProducts > 0 && (
                             <div className={s["product-amount-in-cart"]}>
                                 <p>{productAmountInCart}</p>
                             </div>
